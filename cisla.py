cd889564696ac27242395962554de098bd6355c4
JonasPeichl <144904042+JonasPeichl@users.noreply.github.com>
2023-12-10 22:46:17 +0000

    adventni kalendar den1

--- cisla.py
+++ cisla.py
@@ -1,14 +1,26 @@
 
 text = open("TestTrebuchet")
-
-prvni = False
+a = 0
+b = 0
+c= 0
+d= 0
 for i in range(4):
+   prvni = False
    kod = text.readline()
+   d = d + c
    for line in kod:
       while prvni == False:
          if line.isdigit():
-            print(line)
+            a += int(line)
             prvni = True
+      if line.isdigit():
+         b = 0
+         b += int(line)
+   c = a*10 + b
+   print(c)
+
+print(d)
+         
 
 
       
